dc911607f7f95033d004c653235f88da672cbc35
hongmin3 <hongmin1998@naver.com>
2023-12-23 13:47:32 +0900

    add lecture answer

--- Problem02.py
+++ Problem02.py
@@ -1,8 +1,12 @@
 def solution(score,k):
     answer=0
 
-    for i in range(len(score)):
-        if score[i] >= k:
+#    for i in range(len(score)):
+#        if score[i] >= k:
+#            answer+=1
+
+    for X in score:
+        if X >= k:
             answer+=1
     return answer
 
