974cff7467e8080cf9aa8db392360a43254447c9
hongmin3 <hongmin1998@naver.com>
2024-01-19 01:04:09 +0900

    add revise problem lecture

--- Section_4/Problem_04_01.py
+++ Section_4/Problem_04_01.py
@@ -5,18 +5,27 @@ def solution(nums):
     dy = [0, 1, 0, -1]
     for i in range(len(nums)):
         for j in range(len(nums)):
-            flag = 0
+            # flag = 0
+            flag = True
             for k in range(4):
                 nx = i + dx[k]
                 ny = j + dy[k]
-                if nx < 0 or nx > 4 or ny < 0 or ny > 4:
-                    flag +=1
+            #     if nx < 0 or nx > 4 or ny < 0 or ny > 4:      # 웅덩이인지 찾는 로직
+            #         flag +=1                                  # 가장자리 1000지점을 찾는 로직
 
-                if nx >= 0 and nx < 5 and ny >= 0 and ny < 5:    
-                    if nums[nx][ny] > nums[i][j]:
-                        flag += 1
-            if flag == 4:
-                answer+=1
+            #     if nx >= 0 and nx < 5 and ny >= 0 and ny < 5:  # 4면이 가운데보다 작은지 확인하는 로직
+            #         if nums[nx][ny] > nums[i][j]:
+            #             flag += 1
+            # if flag == 4:
+            #     answer+=1
+                
+            # lecture answer
+                if nx >= 0 and nx < 5 and ny >= 0 and ny < 5 and nums[i][j] >= nums[nx][ny]:
+                    flag=False
+                    break
+            if  flag==True:
+                 answer+=1
+                
     return answer
          
 print(solution([[10, 20, 50, 30, 20], [20, 30, 50, 70, 90], [10, 15, 25, 80, 35], [25, 35, 40, 55, 80], [30, 20, 35, 40, 90]]))
