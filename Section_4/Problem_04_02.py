b99d61fd9f6710bb7e12233a1d00fe836b9b0c88
hongmin3 <hongmin1998@naver.com>
2024-01-22 09:11:44 +0900

    add 4-2 problem answer

---
+++ Section_4/Problem_04_02.py
@@ -0,0 +1,27 @@
+def solution(moves):
+    answer=[0,0]
+    nx = 0
+    ny = 0
+    for i in moves:
+        if i == 'U':
+            nx -= 1
+        elif i == "R" :
+            ny += 1
+        elif i == "L" :
+            ny -= 1
+        elif i == 'D' :
+            nx += 1
+    
+        if nx < 0 :
+            nx = 0
+        if ny < 0 :
+            ny = 0
+        answer = [nx, ny]
+#       print(answer)
+    return answer
+         
+print(solution('RRRDDDLU'))
+print(solution('DDDRRRDDLL'))
+print(solution('RRRRRRDDDDDDUULLL'))
+print(solution('RRRRDDDRRDDLLUU'))
+
