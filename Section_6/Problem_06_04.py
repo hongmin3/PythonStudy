0e3d551000fd0e94cc3aaf0bf8aba76554ad5be1
hongmin3 <hongmin1998@naver.com>
2024-03-11 19:13:19 +0900

    gogo

--- Section_6/Problem_06_04.py
+++ Section_6/Problem_06_04.py
@@ -1,8 +1,7 @@
 def solution(nums, k):
     answer = 0
-
+    cnt=0
     n = len(nums)
-
     for i in range(k+1):
         sum=0
         for j in range(i):
@@ -10,7 +9,7 @@ def solution(nums, k):
         for j in range(n-k+i,n):
             sum+=nums[j]
 
-        if sum > answer:
+        if sum > answer:      
             answer=sum
 
     
@@ -19,7 +18,7 @@ def solution(nums, k):
         
                                          
 print(solution([2, 3, 7, 1, 2, 1, 5], 4))
-print(solution([1, 2, 3, 5, 6, 7, 1, 3, 9], 5))
-print(solution([1, 30, 3, 5, 6, 7], 3))
-print(solution([1, 2, 15, 3, 6, 7, 8, 9], 5))
-print(solution([12, 5, 6, 12, 34, 35, 13, 3, 7, 8, 9], 7))
+#print(solution([1, 2, 3, 5, 6, 7, 1, 3, 9], 5))
+#print(solution([1, 30, 3, 5, 6, 7], 3))
+#print(solution([1, 2, 15, 3, 6, 7, 8, 9], 5))
+#print(solution([12, 5, 6, 12, 34, 35, 13, 3, 7, 8, 9], 7))
